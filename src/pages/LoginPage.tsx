c1aad27ddc21ebf7a1e0783d4b887a611a2095e9
Damir Tagankhozhaev <tagankhozhaev@gmail.com>
2025-12-21 16:29:22 +0500

    fix: changed logo position

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import {Container} from "../ui/Container.tsx";
 
 export const LoginPage:FC = () => {
     return (
-        <Container className={"flex flex-col w-full sm:w-fit gap-[20px] px-4"} >
+        <Container className={"flex flex-col items-center w-full sm:w-fit gap-[20px] px-4"} >
             <img src="logo.png" alt="logo" className="w-full max-w-[200px] mx-auto sm:mx-0"/>
             <LoginForm />
         </Container>
